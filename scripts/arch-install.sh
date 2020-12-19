78fe5bb30a3325685a17a4c21b3a30f499ce5083
Hugoo GB <hugogaben8.02@gmail.com>
2020-12-19 03:07:32 +0100

    reworking themes setup

--- scripts/arch-install.sh
+++ scripts/arch-install.sh
@@ -12,64 +12,17 @@ arch_install() {
   # Install all pkg of the list
   sudo pacman -S --needed - < $HOME/dotfiles/scripts/pacman-pkglist.txt
 
-  # Internet
-  # sudo pacman -S network-manager-applet
-
-  # xorg and lightdm
-  # sudo pacman -S xorg xorg-xinit
-  # sudo pacman -S lightdm lightdm-gtk-greeter qtile firefox
+  # lightdm
   sudo systemctl enable lightdm
 
-  # sudo pacman -S alacritty zsh rofi feh nitrogen
-  # sudo pacman -S python-pip nodejs npm clang
-
-  # Fonts
-  # sudo pacman -S ttf-dejavu ttf-liberation noto-fonts
-
-  # Audio
-  # sudo pacman -S pulseaudio pavucontrol pamixer brightnessctl arandr
-
-  # Storage
-  # sudo pacman -S udiskie ntfs-3g
-
-  # Icons
-  # sudo pacman -S volumeicon cbatticon upower
-
   # Notifications
-  # sudo pacman -S libnotify notification-daemon
   sudo cp -fv $HOME/dotfiles/.local/services/notifications/org.freedesktop.Notifications.service /usr/share/dbus-1/services/
 
-  # Media transfer protocol
-  # sudo pacman -S libmtp
-
-  # File Manager
-  # sudo pacman -S thunar ranger pcmanfm
-
-  # Trash
-  # sudo pacman -S glib2 gvfs
-
-  # Cursor
-  # sudo pacman -S xcb-util-cursor
-
-  # Util
-  # sudo pacman -S unzip tree cloc neofetch fzf the_silver_searcher ripgrep
-
-  # Appearance and composer
-  # sudo pacman -S lxappearance picom
-
-  # Multimedia
-  # sudo pacman -S geeqie vlc okular
-
   # Bluetooth
-  # sudo pacman -S bluez bluez-utils blueman
   sudo systemctl enable bluetooth.service
   sudo cp -fv $HOME/dotfiles/.local/services/bluetooth/main.conf /etc/bluetooth/
 
-  # ASCII art
-  # sudo pacman -S figlet lolcat
-
   # SSH
-  # sudo pacman -S openssh
   sudo systemctl enable sshd
 }
 
@@ -92,21 +45,6 @@ yay_install() {
   # pacman -Qqem > yay-pkglist.txt used to generate the file with all AUR packages installed
   # Install all pkg of the list
   yay -S --needed - < $HOME/dotfiles/scripts/yay-pkglist.txt
-
-  # Fonts
-  # yay -S nerd-fonts-ubuntu-mono nerd-fonts-fira-code
-
-  # Media transfer
-  # yay -S simple-mtpfs
-
-  # vlc dark theme
-  # yay -S vlc-arc-dark-git
-
-  # Visual studio code
-  # yay -S visual-studio-code-bin
-
-  # Brave browser
-  # yay -S brave-bin
 }
 
 arch_setup(){
@@ -118,13 +56,22 @@ arch_setup(){
 
   echo "${PURPLE}Installing material black theme and custom mouse...${RESTORE}"
 
-  sudo cp -rf $HOME/dotfiles/.local/themes/Material-Black-Blueberry /usr/share/themes
-  sudo cp -rf $HOME/dotfiles/.local/themes/Material-Black-Blueberry-Suru /usr/share/icons
-  sudo cp -rf $HOME/dotfiles/.local/themes/Breeze /usr/share/icons
-  sudo cp -rf $HOME/dotfiles/.local/themes/index.theme /usr/share/icons/default
+  mkdir -v $HOME/temp
+  cd $HOME/temp
+  wget -i $HOME/dotfiles/.local/themes/url-themes.txt
+  unzip Material-Black-Blueberry_1.9.1.zip
+  unzip Material-Black-Blueberry-Suru_1.9.1.zip
+  tar -xf 165371-Breeze.tar.gz
+  sudo mv $HOME/temp/Material-Black-Blueberry /usr/share/themes
+  sudo mv $HOME/temp/Material-Black-Blueberry-Suru /usr/share/icons
+  sudo mv $HOME/temp/Breeze /usr/share/icons
+  cd $ACTUAL_DIR
+  rm -rf $HOME/temp
+
+  sudo cp -fv $HOME/dotfiles/.local/themes/index.theme /usr/share/icons/default
 
   # ~/.gtkrc-2.0
-  rm -rf $HOME/.gtkrc-2.0
+  rm $HOME/.gtkrc-2.0
   ln -sv $HOME/dotfiles/.gtkrc-2.0 $HOME/.gtkrc-2.0
   # ~/.config/gtk-3.0/settings.ini
   rm -rf $HOME/.config/gtk-3.0
@@ -136,9 +83,6 @@ arch_setup(){
 lightdm_setup() {
   echo "${PURPLE}Setting up lightdm...${RESTORE}"
 
-  # sudo pacman -S lightdm-webkit2-greeter
-  # yay -S lightdm-webkit-theme-aether
-
   sudo cp -fv $HOME/dotfiles/.config/lightdm/lightdm.conf /etc/lightdm
   sudo cp -fv $HOME/dotfiles/.config/lightdm/lightdm-webkit2-greeter.conf /etc/lightdm
 }
@@ -146,7 +90,6 @@ lightdm_setup() {
 # qtile setup
 qtile_setup() {
   echo "${PURPLE}Installing qtile...${RESTORE}"
-  # sudo pacman -S qtile pacman-contrib
 
   rm -rf $HOME/.config/qtile
   ln -sv $HOME/dotfiles/.config/qtile $HOME/.config/qtile
@@ -155,8 +98,6 @@ qtile_setup() {
 # Rofi setup
 rofi_setup() {
   echo "${PURPLE}Setting up rofi...${RESTORE}"
-
-  # sudo pacman -S papirus-icon-theme
 
   cd $HOME
   git clone https://github.com/davatorium/rofi-themes.git
@@ -195,8 +136,6 @@ alacritty_setup() {
 openbox_setup() {
   echo "${PURPLE}Setting up openbox...${RESTORE}"
 
-  # sudo pacman -S openbox tint2
-
   rm -rf $HOME/.config/openbox
   ln -sv $HOME/dotfiles/.config/openbox $HOME/.config/openbox
   rm -rf $HOME/.config/tint2
@@ -206,8 +145,6 @@ openbox_setup() {
 # Xmonad setup
 xmonad_setup() {
   echo "${PURPLE}Setting up xmonad...${RESTORE}"
-
-  # sudo pacman -S xmonad xmonad-contrib xmobar trayer xdotool
 
   rm -rf $HOME/.config/xmonad
   ln -sv $HOME/dotfiles/.config/xmonad $HOME/.config/xmonad
@@ -223,8 +160,6 @@ xmonad_setup() {
 # Installing neovim latest release
 nvim_nightly() {
   echo "${PURPLE}Installing nvim-nightly...${RESTORE}"
-
-  # yay -S neovim-nightly-git
 }
 
 # LSP things, vim plug, ohmyzsh and link dotfiles
